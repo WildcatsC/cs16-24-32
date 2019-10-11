8ff7ea616f66d1ff3ce8ee7138429e514cdec24a
WildcatsC <octsteven@gmail.com>
2019-10-10 23:36:56 -0700

    memory leak and constructor

--- 24/Linkedlist/llist.cpp
+++ 24/Linkedlist/llist.cpp
@@ -11,6 +11,14 @@ llist::llist(const llist& other){
 }
 
 llist::append(int x){
-	Node* n = new Node;  // on the stack;
-	n->info = 
+	Node* n = new Node;  // on the heap;
+	n->info = NULL;
+
+	if(head == NULL && tail == NULL){ //append 到一个空的llist
+		head = n;
+		tail = n;	
+	}else{
+		tail->next = n;
+		
+	}
 }
