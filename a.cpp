75be26bfdf9ca7138f5b26fe64db08ecb01b809a
WildcatsC <octsteven@gmail.com>
2019-11-15 14:57:38 -0800

    11-15

---
+++ a.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+struct N{public:int k;
+};
+
+int main(){
+//	N* p = new N;
+//	N q;
+	//p->k = 1;
+//	q.k = 2;
+	//cout << p->k << endl;
+	int k = true;
+	int p = 123/10;
+	cout << p << endl;
+	return 0;
+}
