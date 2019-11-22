b141037c02a59c437bdc204ee2b44c1769999701
WildcatsC <octsteven@gmail.com>
2019-11-21 23:25:38 -0800

    11-21 lab04

--- test.cpp
+++ test.cpp
@@ -3,21 +3,25 @@ using namespace std;
 
 class L{
 public:
+	L(int k):k(k){ cout << "initialize!" << endl;}
+
 	void func1(){
 		cout << "f1" << endl;
 	}
 	void func2(){
 		this->func1();
 	}	
-
+	int k;
 
 };
 
 
 int main(){
-	L *l;
-	l = new L;
-	l->func2();
+	// L *l;
+	// l = new L;
+	// l->func2();
+	L k(100);
+	cout << k.k << endl;
 	return 0;
 
 }
