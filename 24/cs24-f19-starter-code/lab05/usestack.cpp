c1d8b95c829ff41242cb7efcdd387202b1d2f96b
WildcatsC <octsteven@gmail.com>
2019-12-05 16:08:45 -0800

    .

--- 24/cs24-f19-starter-code/lab05/usestack.cpp
+++ 24/cs24-f19-starter-code/lab05/usestack.cpp
@@ -14,7 +14,7 @@ int profix(){   //   profix algorithm
 int main(int argc, char* argv[]){   // 遗漏小知识点  char** argv and char* argv[]
     Stack s;
 
-    cout<< argv[0] <<endl; 
+    cout<< (int)argv[0] <<endl; 
 
     s.push(10);
     s.push(20);
