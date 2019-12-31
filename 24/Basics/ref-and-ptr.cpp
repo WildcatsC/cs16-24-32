05222358e8a032a1ec68e6d03685e91ebb0277e6
WildcatsC <octsteven@gmail.com>
2019-12-31 13:09:51 +0800

    .

--- 24/Basics/ref-and-ptr.cpp
+++ 24/Basics/ref-and-ptr.cpp
@@ -7,12 +7,12 @@ struct N{public:int k;
 
 int main(){
 	N* p = new N;
-//	N q;
-	//p->k = 1;
-//	q.k = 2;
+	N q;
+
+	p->k = 1;
+	q.k = 2;
+
 	cout << p->k << endl;
-	int k = true;
-//	int p = 123/10;
-	cout << p << endl;
+	cout << q.k << endl;
 	return 0;
 }
