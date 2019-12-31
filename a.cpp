0fab2291cd40bde71039d315911bf085bebdf017
WildcatsC <octsteven@gmail.com>
2019-12-31 13:06:25 +0800

    .

--- a.cpp
+++ a.cpp
@@ -6,13 +6,13 @@ struct N{public:int k;
 };
 
 int main(){
-//	N* p = new N;
+	N* p = new N;
 //	N q;
 	//p->k = 1;
 //	q.k = 2;
-	//cout << p->k << endl;
+	cout << p->k << endl;
 	int k = true;
-	int p = 123/10;
+//	int p = 123/10;
 	cout << p << endl;
 	return 0;
 }
