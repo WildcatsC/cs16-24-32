294dbe14116ad1bba2d955c23d999fc196f714dc
WildcatsC <octsteven@gmail.com>
2020-01-13 12:50:28 -0800

    .

--- 32/Containers/set.cpp
+++ 32/Containers/set.cpp
@@ -16,3 +16,22 @@ int main() {
 }
 // default: alpha. order 
 // <unordered_set>
+for(set<string>::iterator i = s.begin(); i < s.end(); i++){
+	cout << *i << << endl;
+}
+
+// find() returns an iterator to the item in a set if it exists.
+// 		Otherwise, find() returns an iterator == set.end()
+
+if (s.find("Harry") != s.end()) {
+	cout << "Harry exists!" << endl; // prints this
+} else {
+	cout << "Harry DNE" << endl;
+}
+
+if (s.find("Hagrid") != s.end()) {
+	cout << "Hagrid exists!" << endl;
+} else {
+	cout << "Hagrid DNE" << endl;	// prints this
+}
+
