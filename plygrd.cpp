d827881d92310f05781800eae31af17c4f4ed53f
WildcatsC <octsteven@gmail.com>
2020-01-15 14:57:10 -0800

    .

--- plygrd.cpp
+++ plygrd.cpp
@@ -15,13 +15,31 @@ public:
 
 };
 
+class Z {
+    public:
+    char a;     // 1 byte
+    int b;      // 4
+	long d;
+    char c;     // 1
+    //double d;   // 8
+};
 
-int main(){
+int main() {
+    Z z;
+    cout << sizeof(z) << endl;                                  // 24
+    cout << "&z = " << &z << endl;                              // 0x7ffee07c1700
+    cout << "&z.a = " << reinterpret_cast<void*>(&z.a) << endl; // 0x7ffee07c1700
+    cout << "&z.b = " << &z.b << endl;                          // 0x7ffee07c1704
+    cout << "&z.c = " << reinterpret_cast<void*>(&z.c) << endl; // 0x7ffee07c1708
+    cout << "&z.d = " << &z.d << endl;                          // 0x7ffee07c1710
+}
+
+//int main(){
 	// L *l;
 	// l = new L;
 	// l->func2();
-	L k(100);
-	cout << k.k << endl;
-	return 0;
+//	L k(100);
+//	cout << k.k << endl;
+//	return 0;
 
-}
+//}
