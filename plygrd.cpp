6d74d1da5057e0e1837ec4d70fb729e0468bdc0a
WildcatsC <octsteven@gmail.com>
2020-01-22 13:05:55 -0800

    .

--- plygrd.cpp
+++ plygrd.cpp
@@ -2,8 +2,19 @@
 using namespace std;
 
 class L{
+private:
+	int k,l;
 public:
-	L(int k):k(k){ cout << "initialize!" << endl;}
+	L(int k, int l):k(k),l(l){ 
+	cout << "k:" << k << endl;
+	cout << "l:" << l << endl;
+	int t = k;
+	k = l; 
+	l = t;
+	cout << "k:" << k << endl;
+	cout << "l:" << l << endl;
+	cout << "initialize!" << endl;
+	}
 
 	void func1(){
 		cout << "f1" << endl;
@@ -11,7 +22,6 @@ public:
 	void func2(){
 		this->func1();
 	}	
-	int k;
 
 };
 
@@ -29,24 +39,21 @@ int main() {
     cout << sizeof(z) << endl;                                  // 24
     cout << "&z = " << &z << endl;                              // 0x7ffee07c1700
     cout << "&z.a = " << reinterpret_cast<void*>(&z.a) << endl; // 0x7ffee07c1700
+// char shoud use reinterpret cast to print
     cout << "&z.b = " << &z.b << endl;                          // 0x7ffee07c1704
     cout << "&z.c = " << reinterpret_cast<void*>(&z.c) << endl; // 0x7ffee07c1708
     cout << "&z.d = " << &z.d << endl;                          // 0x7ffee07c1710
-}
 
 //int main(){
 	// L *l;
 	// l = new L;
 	// l->func2();
-<<<<<<< HEAD
 //	L k(100);
 //	cout << k.k << endl;
 //	return 0;
 
 //}
-=======
-	L k(100);
-	cout << k.k << endl;
+	L k(100,10000);
+	cout << "====" << endl;
 	return 0;
 }
->>>>>>> 42e3e37c31a6407b0dc4a686f81d05b3d0e0b135
