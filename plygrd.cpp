9ca9b92882bf3a32288af1f2f6165db5629612f5
WildcatsC <octsteven@gmail.com>
2020-02-05 17:34:52 -0800

    .

--- plygrd.cpp
+++ plygrd.cpp
@@ -5,6 +5,12 @@ class L{
 private:
 	int k,l;
 public:
+	L(){
+	cout << "default:" << endl;
+	this->k = 1;
+	this->l = 2;
+	}
+
 	L(int k, int l):k(k),l(l){ 
 	cout << "k:" << k << endl;
 	cout << "l:" << l << endl;
@@ -13,11 +19,12 @@ public:
 	l = t;
 	cout << "k:" << k << endl;
 	cout << "l:" << l << endl;
-	cout << "initialize!" << endl;
+	cout << "not default initialize!" << endl;
 	}
 
 	void func1(){
 		cout << "f1" << endl;
+		cout << this->k << endl;
 	}
 	void func2(){
 		this->func1();
@@ -50,6 +57,13 @@ int main() {
 	a = new int[100];
 	for(int i = 0; i<4; i++) a[i]=i;
 	cout << a[0]<<a[1]<<a[2]<<a[3] << endl;
-	cout << (a+1)[2] << endl;
+	cout << (a+1)[0] << endl;
+
+	cout << "===" << endl;
+	L *l;
+	l = new L();
+	l->func1();
+	l = new L(3,4);
+	l->func1();
 	return 0;
 }
