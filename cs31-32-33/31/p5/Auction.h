c203de02bce7fba257551b0e1f4a62cf6c4acfc9
WildcatsC <octsteven@gmail.com>
2020-08-04 22:27:19 +0800

    .

---
+++ cs31-32-33/31/p5/Auction.h
@@ -0,0 +1,26 @@
+#ifndef AUCTION_H
+#define AUCTION_H
+
+#include <string>
+
+class Auction{
+    private:
+    std::string mDescription;
+    double mMinimumPrice;
+    double mCurrentBid;
+    int mTotalNumberOfBids;
+    bool mAuctionIsOpen;
+    
+    public:
+    Auction(std::string description, double minprice);
+    void openAuction();
+    bool bid(double amount);
+    int numberOfBids() const;
+    void closeAuction();
+    bool wasSuccessful() const;
+    double winningBid() const;
+    std::string getDescription() const;
+
+};
+
+#endif
---
+++ cs31-32-33/31/p5/Test.cpp
@@ -0,0 +1,9 @@
+#include "Auction.h"
+#include "EBayBucks.h"
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    return 0;
+}
